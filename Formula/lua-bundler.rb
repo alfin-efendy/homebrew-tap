d2f67e98cebbf534f163cec9c4e91394888a293a
GitHub Action <action@github.com>
2026-04-03 04:32:30 +0000

    Update lua-bundler to 1.10.0

--- Formula/lua-bundler.rb
+++ Formula/lua-bundler.rb
@@ -1,25 +1,25 @@
 class LuaBundler < Formula
   desc "Lua script bundler for Roblox development"
   homepage "https://github.com/alfin-efendy/lua-bundler"
-  version "1.9.0"
+  version "1.10.0"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.9.0/lua-bundler-darwin-arm64"
-      sha256 "03841399098bd7cea5b439777f27e3ce81308269c8877126bda9a6cff962174a"
+      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.10.0/lua-bundler-darwin-arm64"
+      sha256 "9aa026a765c3c5c4c5009e8a4364b4bbb1a52922c52d0270959e76cfe7068947"
     else
-      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.9.0/lua-bundler-darwin-amd64"
-      sha256 "a54be689714cf4e8e7a65ea55be22853e24d3fc3afa16351cc25e77b39a17922"
+      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.10.0/lua-bundler-darwin-amd64"
+      sha256 "ae30ca4d9bf9232550f2f9473572e78a83508498ec48eee911981c733371e605"
     end
   end
   
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.9.0/lua-bundler-linux-arm64"
-      sha256 "ee875e6b47301a2402fa4e472e200172c1e26ec59155fdf09daf90bc712ccb7a"
+      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.10.0/lua-bundler-linux-arm64"
+      sha256 "0f2e39ac5b2f4e233a959bffa654cc8165b5c4ce8fdda3f48c4606b49a522102"
     else
-      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.9.0/lua-bundler-linux-amd64"
-      sha256 "80ef2d8c5a3ec647d59f4e385f2d2e8950c5304a419090c97fe89e9e3a24d865"
+      url "https://github.com/alfin-efendy/lua-bundler/releases/download/v1.10.0/lua-bundler-linux-amd64"
+      sha256 "79b0467ea71133a36fc8f58bb9ddbf03f7ccf5d7161415e497958939e3d69744"
     end
   end
 
